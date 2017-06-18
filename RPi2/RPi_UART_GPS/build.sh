7faa591eb8987518d8453df92be35df02fef3f38
Edward Vidal Jr <develone@sbcglobal.net>
2017-06-18 11:41:20 +0000

    compiles checksum_ultibo.c creates the static libgps.a

--- RPi2/RPi_UART_GPS/build.sh
+++ RPi2/RPi_UART_GPS/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-rm -f kernel7.img
+rm -f kernel7.img checksum_ultibo.o libgps.a 
+echo 'Compiling checksum_ultibo.c' 
+arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c checksum_ultibo.c
+echo 'Creating libgps.a'
+arm-none-eabi-ar rcs libgps.a checksum_ultibo.o
 echo 'Compiling SerialConnection.lpr'
 /home/pi/ultibo/core/fpc/bin/fpc -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/pi/ultibo/core/fpc/bin/rpi2.cfg -O2 SerialConnection.lpr
 date
-ls -la kernel7.img
+ls -la kernel7.img checksum_ultibo.o libgps.a

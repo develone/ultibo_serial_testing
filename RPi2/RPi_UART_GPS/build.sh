94999eb65ea87f4169a1fa9cdb6e0b577a956659
Edward Vidal Jr <develone@sbcglobal.net>
2017-06-19 22:29:08 +0000

    adding -vi option when compiling

--- RPi2/RPi_UART_GPS/build.sh
+++ RPi2/RPi_UART_GPS/build.sh
@@ -5,6 +5,6 @@ arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-a
 echo 'Creating libgps.a'
 arm-none-eabi-ar rcs libgps.a checksum_ultibo.o
 echo 'Compiling SerialConnection.lpr'
-/home/pi/ultibo/core/fpc/bin/fpc -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/pi/ultibo/core/fpc/bin/rpi2.cfg -O2 SerialConnection.lpr
+/home/pi/ultibo/core/fpc/bin/fpc -vi -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/pi/ultibo/core/fpc/bin/rpi2.cfg -O2 SerialConnection.lpr
 date
 ls -la kernel7.img checksum_ultibo.o libgps.a

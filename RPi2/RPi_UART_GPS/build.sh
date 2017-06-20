6f0db029828e442ff6585b11a9ee664130054858
Edward Vidal Jr <develone@sbcglobal.net>
2017-06-20 23:18:59 +0000

    Now compiles UDPServerGPS.lpr

--- RPi2/RPi_UART_GPS/build.sh
+++ RPi2/RPi_UART_GPS/build.sh
@@ -4,7 +4,9 @@ echo 'Compiling checksum_ultibo.c'
 arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -c checksum_ultibo.c
 echo 'Creating libgps.a'
 arm-none-eabi-ar rcs libgps.a checksum_ultibo.o
-echo 'Compiling SerialConnection.lpr'
-/home/pi/ultibo/core/fpc/bin/fpc -vi -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/pi/ultibo/core/fpc/bin/rpi2.cfg -O2 SerialConnection.lpr
+#echo 'Compiling SerialConnection.lpr'
+#/home/pi/ultibo/core/fpc/bin/fpc -vi -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/pi/ultibo/core/fpc/bin/rpi2.cfg -O2 SerialConnection.lpr
+echo 'Compiling UDPServerGPS.lpr'
+/home/pi/ultibo/core/fpc/bin/fpc -vi -B -Tultibo -Parm -CpARMV7A -WpRPI2B @/home/pi/ultibo/core/fpc/bin/rpi2.cfg -O2 UDPServerGPS.lpr
 date
 ls -la kernel7.img checksum_ultibo.o libgps.a
